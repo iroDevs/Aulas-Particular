034c794bed909080a8b2e786d7d3a8bc84729038
PedroDev <pedrohenriquelouresoliveira@gmail.com>
2025-06-07 19:13:28 -0300

    Coomit put e get

--- src/server.ts
+++ src/server.ts
@@ -20,23 +20,38 @@ const Users = [
 ]
 
 
+type UserBodyCreate = {
+    name: string;
+}
+
 // const users = await axios.get('http://localhost:3000/users')
 
 //Primeira rota
 app.get('/users', async (request, reply) => {
-    return reply.status(200).send(Users)
+        return reply.status(200).send(Users)
+})
+
+
+app.get('/users/:id', async (request, reply) => {
+    const { id } = request.params as { id: string }
+    const user = Users.find(user => user.id === Number(id))
+    if (!user) {
+        return reply.status(404).send({ message: 'User not found' })
+    }
+    return reply.status(200).send(user)
 })
 
 app.post('/users', async (request, reply) => {
-    const body = request.body as { name: string }
+    const body = request.body as UserBodyCreate
     const newUser = {
         id: Users.length + 1,
         name: body.name,
     }
     Users.push(newUser)
     return reply.status(201).send(newUser)
-
 })
+
+
 
 
 function start(){
