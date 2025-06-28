068ae0155c823f93b07de460555d8a79420ee1d7
PedroDev <pedrohenriquelouresoliveira@gmail.com>
2025-06-28 18:40:16 -0300

    Commit

--- src/server.ts
+++ src/server.ts
@@ -8,10 +8,6 @@ app.get('/api', (req, res)=>{
     return res.status(200).send({message: "oi mundo"})
 })
 
-
-
-
-
 function start(){
     app.listen({port: 3001}, (error)=>{
         console.log('servidor rodando na porta 3001');
